05f1ddb464049157b75b037224c8ac23d9ff7286
Luis Felipe Dos Santos <130870829+Luis5Felipe@users.noreply.github.com>
2026-02-05 19:15:39 -0300

    aula 01

---
+++ aula-01/script.js
@@ -0,0 +1,9 @@
+const mario = document.querySelector(".mario");
+const cano = document.querySelector(".cano");
+const reset = document.querySelector(".reset");
+const cima = document.querySelector(".cima");
+const gameover = document.querySelector(".gameover");
+
+const pular = () => {
+  mario.classList.add("pulo");
+};
